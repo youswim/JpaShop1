de559834d556c25701f29a35c4d1575aa89279b3
youswim <youswim96@gmail.com>
2022-06-29 23:55:14 +0900

    60

--- src/main/java/jpabook/jpashop1/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop1/repository/OrderRepository.java
@@ -1,10 +1,12 @@
 package jpabook.jpashop1.repository;
 
 import jpabook.jpashop1.domain.Order;
+import jpabook.jpashop1.domain.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -20,7 +22,23 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch) {
-//
-//    }
+    public List<Order> findAll(OrderSearch orderSearch) {
+        return em.createQuery("select o from Order o join o.member m" +
+                        " where o.status = :status" +
+                        " and m.name like : name", Order.class)
+                .setParameter("status", orderSearch.getOrderStatus())
+                .setParameter("name", orderSearch.getMemberName())
+                .setMaxResults(1000)
+                .getResultList();
+    }
+
+    public List<Order> findAllByString(OrderSearch orderSearch) {
+
+        String jpql = "select o From Order o join o.member m";
+        boolean isFirstCondition = true;
+
+        if (orderSearch.getOrderStatus() != null) {
+
+        }
+    }
 }
